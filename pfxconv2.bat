7c79e002bc396f7dcbe3844d8310395c77b7d4d2
Nik Kesic <nkesic@rs.com>
2017-04-11 06:56:51 -0600

    pfxconv2 menu changed
    make the menu more robust and added the powershell password function

--- pfxconv2.bat
+++ pfxconv2.bat
@@ -2,7 +2,7 @@
 :
 : Rocket Software Confidential
 : OCO Source Materials
-: Copyright (C) Rocket Software. 2009, 2010, 2011, 2012, 2013
+: Copyright (C) Rocket Software. 2017
 : 
 :
 : @(:) $0 : MS exported certificate store pfx file to pkcs#8 file 
@@ -11,7 +11,7 @@
 :	   : U2 Support Denver - USA
 : Synopsis:
 :
-:     pfxconv2
+: rem name pfxconv2
 :
 :         for Windows 2008, 7, - 64 bit
 :           
@@ -27,22 +27,36 @@ echo.  PFX Certificate Store Converter
 echo. +++++++++++++++++++++++++++++++++
 echo.      
 :
-set /p INP1=Enter name of the PFX file :  
-IF  NOT (%INP1%)==() (
-set pfxfile=%INPUT%
-) ELSE (
-echo " bad input"
-)
-set /p INP2=Enter new name for the server root certificate :  
-IF  NOT (%INP2%)==() (
-set rootfile=%INP2%
-) ELSE (
-echo " bad input"
-)
-openssl pkcs12 -cacerts -in %INP1% -nodes -out %INP2%
-::openssl pkcs12 -cacerts -in %INP1% -nodes | openssl x509 >> %INP2%.crt
-:: future extraction to remove headers
-:: openssl x509 -in %INP2%.crt > %INP2%.cer
+:pfxconv2
+set /p pfxfile=Enter name of the PFX file : %pfxfile%
+IF "%pfxfile%"=="" goto Errorpfxconv1
+goto rootName
+:Errorpfxconv1
+echo Bad Input!!
+goto pfxconv2
+:
+:rootName
+set /p rootName=Enter name for the server root certificate : %rootName%
+IF "%rootName%"=="" goto ErrorrootName
+goto pvtPass
+:ErrorrootName
+echo Bad Input!!
+goto rootName
+:
+:pvtPass
+set "psCommand=powershell -Command "$pword = read-host 'Enter Password' -AsSecureString ; ^
+    $BSTR=[System.Runtime.InteropServices.Marshal]::SecureStringToBSTR($pword); ^
+        [System.Runtime.InteropServices.Marshal]::PtrToStringAuto($BSTR)""
+for /f "usebackq delims=" %%p in (`%psCommand%`) do set pvtPass=%%p
+IF "%pvtPass%"=="" goto ErrorpvtPass
+goto nnext
+:ErrorpvtPass
+echo Bad Input!!
+goto pvtPass
+:nnext
+:
+openssl pkcs12 -cacerts -in %pfxfile% -nodes -out %rootName% -passin pass:%pvtPass%
+:END
 echo.
 SET /P M= Any key to exit : 
 IF %M%== GOTO EOF
